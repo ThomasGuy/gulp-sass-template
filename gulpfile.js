7c2eff7b72f62467efe48a34e4466740048c2a2c
Thomas Guiy <twguy66@gmail.com>
2017-09-06 04:11:10 +0100

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp');
+
+// Require plugins
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+var runSequence = require('run-sequence');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('prefixer', function() {
+  return gulp.src('app/css/*.css')
+    .pipe(autoprefixer({
+      browsers:['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('sass', function () {
+  return gulp.src('app/sass/**/*.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  });
+});
+
+// Gulp watch syntax
+gulp.task('watch', function() {
+  gulp.watch('app/sass/**/*.sass', ['sass']);
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+// Optimizing Images
+gulp.task('images', function() {
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    // Caching images that ran through imagemin
+    .pipe(cache(imagemin({
+      interlaced: true,
+    })))
+    .pipe(gulp.dest('dist/images'))
+});
+
+// Copying fonts
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+})
+
+// Build Sequences
+// ---------------
+
+gulp.task('default', function(callback) {
+  runSequence(['sass', 'prefixer', 'browserSync'], 'watch',
+    callback
+  )
+})
