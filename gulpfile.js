2bbaf0d77d6379666f008b63008a93391885eca9
Thomas Guy <twguy66@gmail.com>
2018-01-04 20:43:15 +0700

    linter tidy up

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
-let gulp = require('gulp');
+const gulp = require('gulp');
 
 // Require plugins
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var imagemin = require('gulp-imagemin');
-var runSequence = require('run-sequence');
-var autoPrefixer = require('gulp-autoprefixer');
-var jshint = require('gulp-jshint');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var uglifycss = require('gulp-uglifycss');
-var connect = require('connect');
-var serve = require('serve-static');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var plumber = require('gulp-plumber');
-var beeper = require('beeper');
-var del = require('del');
-var sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const imagemin = require('gulp-imagemin');
+const runSequence = require('run-sequence');
+const autoPrefixer = require('gulp-autoprefixer');
+const jshint = require('gulp-jshint');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const uglifycss = require('gulp-uglifycss');
+const connect = require('connect');
+const serve = require('serve-static');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const plumber = require('gulp-plumber');
+const beeper = require('beeper');
+const del = require('del');
+const sourcemaps = require('gulp-sourcemaps');
 
-const path= {
+const path = {
   style: {
     src: 'app/css/*.css',
     dest: 'dist/css/'
@@ -61,104 +61,83 @@ function onError(err) {
 }
 
 // Gulp compile sass
-gulp.task('sass', function() {
-  return gulp.src(path.sass.src)
-    .pipe(plumber({
-      errorHandler: onError
-    }))
-    .pipe(sass())
-    .pipe(autoPrefixer())
-    .pipe(gulp.dest(path.sass.dest))
+gulp.task('sass', () => gulp.src(path.sass.src)
+  .pipe(plumber({
+    errorHandler: onError
+  }))
+  .pipe(sass())
+  .pipe(autoPrefixer())
+  .pipe(gulp.dest(path.sass.dest)));
 
-});
-
-gulp.task('html', function() {
-  return gulp.src(path.html.src)
-    .pipe(gulp.dest(path.html.dest))
-});
+gulp.task('html', () => gulp.src(path.html.src)
+  .pipe(gulp.dest(path.html.dest)));
 
 // Styles Task
-gulp.task('styles', function() {
-  return gulp.src([path.style.src])
-    .pipe(concat('main.css'))
-    .pipe(uglifycss({
-      "maxLineLen": 120,
-      "uglyComments": true
-    }))
-    .pipe(gulp.dest(path.style.dest));
-});
+gulp.task('styles', () => gulp.src([path.style.src])
+  .pipe(concat('main.css'))
+  .pipe(uglifycss({
+    maxLineLen: 120,
+    uglyComments: true
+  }))
+  .pipe(gulp.dest(path.style.dest)));
 
 // Gulp script test, combine and minify
-gulp.task('script', function() {
-  return gulp.src(path.script.src)
-    .pipe(sourcemaps.init())
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('all.js'))
-    .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(path.script.dest));
-});
+gulp.task('script', () => gulp.src(path.script.src)
+  .pipe(sourcemaps.init())
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'))
+  .pipe(concat('all.js'))
+  .pipe(uglify())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(path.script.dest)));
 
 // Images Task
-gulp.task('images', function() {
-  return gulp.src(path.image.src)
-    .pipe(imagemin())
-    .pipe(gulp.dest(path.image.dest));
-});
+gulp.task('images', () => gulp.src(path.image.src)
+  .pipe(imagemin())
+  .pipe(gulp.dest(path.image.dest)));
 
-gulp.task('fonts', function() {
-  return gulp.src(path.font.src)
-    .pipe(gulp.dest(path.font.dest));
-});
+gulp.task('fonts', () => gulp.src(path.font.src)
+  .pipe(gulp.dest(path.font.dest)));
 
 // Gulp browserSync
-gulp.task('browserSync', function(cb) {
-  return browserSync({
-    server: {
-      baseDir: 'app'
-    }
-  }, cb);
-});
+gulp.task('browserSync', cb => browserSync({
+  server: {
+    baseDir: 'app'
+  }
+}, cb));
 
-gulp.task('browserify', function() {
-  return browserify('./app/js/app.js')
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('dist/js'));
-})
+gulp.task('browserify', () => browserify('./app/js/app.js')
+  .bundle()
+  .pipe(source('bundle.js'))
+  .pipe(gulp.dest('dist/js')));
 
 // node Staic server
-gulp.task('server', function() {
-  return connect().use(serve(__dirname))
-    .listen(8080)
-    .on('listening', function() {
-      console.log('Server running on port:8080');
-    });
-});
+gulp.task('server', () => connect().use(serve(__dirname))
+  .listen(8080)
+  .on('listening', () => {
+    console.log('Server running on port:8080');
+  }));
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
   gulp.watch(path.sass.src, ['sass', browserSync.reload]);
   gulp.watch(path.html.src, browserSync.reload);
   gulp.watch(path.script.src, ['script', browserSync.reload]);
 });
 
-gulp.task('autoPrefix', function() {
-  return gulp.src(path.autoPrefix.src)
-    .pipe(autoPrefixer())
-    .pipe(gulp.dest(path.autoPrefix.dest));
-});
+gulp.task('autoPrefix', () => gulp.src(path.autoPrefix.src)
+  .pipe(autoPrefixer())
+  .pipe(gulp.dest(path.autoPrefix.dest)));
 
 // Build Sequences
 // ---------------
-gulp.task('default', function(cb) {
+gulp.task('default', cb => {
   runSequence('sass', 'browserSync', 'watch', cb);
 });
 
-gulp.task('clean', function(cb) {
+gulp.task('clean', cb => {
   del(['dist'], cb);
 });
 
-gulp.task('build', function(cb) {
-  runSequence( 'sass', 'script', 'images', 'styles', 'fonts', 'html', cb);
+gulp.task('build', cb => {
+  runSequence('sass', 'script', 'images', 'styles', 'fonts', 'html', cb);
 });
