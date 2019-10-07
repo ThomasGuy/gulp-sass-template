d8e852330592cf5f11f8ef061e1eab84c09f9697
ThomasGuy <TWGuy66@gmail.com>
2019-10-07 16:19:24 +0700

    update to gulp 4

---
+++ gulpfile.babel.js
@@ -0,0 +1,84 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+const del = require('del');
+
+sass.compiler = require('node-sass');
+
+const paths = {
+  styles: {
+    src: 'app/css/*.css',
+    dest: 'dist/css/',
+  },
+  scripts: {
+    src: 'app/js/**/*.js',
+    dest: 'dist/js/',
+  },
+  sass: {
+    src: 'app/css/sass/**/*.sass',
+    dest: 'app/css/',
+  },
+  image: {
+    src: 'app/img/**/*.*',
+    dest: 'dist/img/',
+  },
+  autoPrefix: {
+    src: 'app/css/**/*.css',
+    dest: 'app/css/',
+  },
+  html: {
+    src: 'app/*.html',
+    dest: 'dist/',
+  },
+  font: {
+    src: 'app/fonts/*.*',
+    dest: 'dist/fonts/',
+  },
+};
+
+/*
+ * For small tasks you can export arrow functions
+ */
+export const clean = () => { return del(['assets']); };
+
+/*
+ * You can also declare named functions and export them as tasks
+ */
+export function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(sass())
+    .pipe(cleanCSS())
+    // pass in options to the stream
+    .pipe(rename({
+      basename: 'main',
+      suffix: '.min',
+    }))
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+export function scripts() {
+  return gulp.src(paths.scripts.src, { sourcemaps: true })
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+/*
+ * You could even use `export as` to rename exported tasks
+ */
+function watchFiles() {
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.styles.src, styles);
+}
+export { watchFiles as watch };
+
+const build = gulp.series(clean, gulp.parallel(styles, scripts, watchFiles));
+/*
+* Export a default task
+*/
+export default build;
